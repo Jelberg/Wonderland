e9b84a93ba009635dbd61a18b4aefab153dfc645
Jelberg <jessica_elberg@hotmail.com>
2014-12-10 10:15:04 -0430

    arreglo la busqueda por lista y carga de imagenes
    se cargan y guardan imagenes solo de productos

---
+++ Wonderland 1.2/src/modelo/Administrador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+/**
+ *
+ * @author essteeli
+ */
+public class Administrador {
+    private String Tipo;
+
+    public Administrador(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+    
+}
