7df23519c08aaf411837ef4ac496bda91c9c373f
mariaEPerezF <esteperez1593@gmail.com>
2014-12-10 02:08:43 -0430

    Wonderland1.3

---
+++ Wonderland 2.0/build/classes/vista/AdministradorProductos.rs
@@ -0,0 +1,9 @@
+AdministradorProductos$1
+AdministradorProductos$4
+AdministradorProductos$5
+AdministradorProductos$2
+AdministradorProductos$3
+AdministradorProductos$8
+AdministradorProductos
+AdministradorProductos$6
+AdministradorProductos$7
